091cc4940fd109823f3f0200201c3817aebdaf1f
pratham6767 <prathampatel8316@gmail.com>
2024-02-07 19:16:12 +0530

    lib

---
+++ models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+
+const UserSchema = new Schema({
+    
+});
