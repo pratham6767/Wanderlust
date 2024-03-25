095d597ae1afba33287423264a5f2554d65c6288
pratham6767 <prathampatel8316@gmail.com>
2024-03-25 21:43:30 +0530

    style changes

--- init/index.js
+++ init/index.js
@@ -19,6 +19,7 @@ async function main() {
 const initDB = async () => {
   await Listing.deleteMany({});
   initData.data=initData.data.map((obj)=>({...obj,owner:"65c5df1286ec2f42be96157a"}));
+  
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
